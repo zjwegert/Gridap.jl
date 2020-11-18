f5533001be2d9503804852f731bebc102f546445
eneiva <eneiva@cimne.upc.edu>
2020-11-18 10:16:10 +0100

    [IntegratedLegendreBasesTests] Completed batch of tests

--- test/PolynomialsTests/IntegratedLegendreBasesTests.jl
+++ test/PolynomialsTests/IntegratedLegendreBasesTests.jl
@@ -6,47 +6,33 @@ using Gridap.Fields
 using Gridap.Polynomials
 
 import Gridap.Polynomials: _legendre
-# import Gridap.Polynomials: _evaluate_1d_il!, _gradient_1d_il!, _hessian_1d_il!
+import Gridap.Fields: Broadcasting
 
 @test all(map(i->map(x->_legendre(x,Val{i}()),(-1,1))==(1-2*(i%2),1),0:4))
 
-# D = 3
-# order = 4
-# n = order + 1
+# Real-valued Q space with isotropic order
 
-# x = Point(0,0.5,1)
-# V = zeros(Float64,(D,n))
-# G = zeros(Float64,(D,n))
-# H = zeros(Float64,(D,n))
+xi = Point(0.5)
+V = Float64
+G = gradient_type(V,xi)
+H = gradient_type(G,xi)
+order = 3
 
-# for d = 1:D
-#   _evaluate_1d_il!(V,x,order,d)
-#   _gradient_1d_il!(G,x,order,d)
-#   _hessian_1d_il!(H,x,order,d)
-# end
+b = IntegratedLegendreBasis{1}(V,order)
+∇b = Broadcasting(∇)(b)
+∇∇b = Broadcasting(∇)(∇b)
 
-# order 0 degenerated case
+solv = evaluate(b,Point{1,V}[(0.5),])
+solg = evaluate(∇b,Point{1,V}[(0.5),])
+solh = evaluate(∇∇b,Point{1,V}[(0.5),])
 
-# xi = Point(2,3)
-# np = 5
-# x = fill(xi,np)
+v = V[0.5  0.5  -sqrt(3)/4 0.0]
+g = G[-1.0 1.0 0.0 -sqrt(5)/2]
+h = H[0.0 0.0 2*sqrt(3) 0.0]
 
-# order = 0
-# V = Float64
-# G = gradient_type(V,xi)
-# H = gradient_type(G,xi)
-# b = MonomialBasis{2}(V,order)
-# @test get_order(b) == 0
-# @test get_orders(b) == (0,0)
-
-# v = V[1.0,]
-# g = G[(0.0, 0.0),]
-# h = H[(0.0, 0.0, 0.0, 0.0),]
-
-# bx = repeat(permutedims(v),np)
-# ∇bx = repeat(permutedims(g),np)
-# Hbx = repeat(permutedims(h),np)
-# test_field_array(b,x,bx,grad=∇bx,gradgrad=Hbx)
+@test evaluate(b,[xi,]) ≈ v
+@test evaluate(∇b,[xi,]) ≈ g
+@test evaluate(∇∇b,[xi,]) ≈ h
 
 # Real-valued Q space with isotropic order
 
@@ -74,14 +60,19 @@ test_field_array(b,x,bx,grad=∇bx,gradgrad=Hbx)
 orders = (1,2)
 V = Float64
 G = gradient_type(V,xi)
+H = gradient_type(G,xi)
+
 b = IntegratedLegendreBasis{2}(V,orders)
+∇b = Broadcasting(∇)(b)
+∇∇b = Broadcasting(∇)(∇b)
 
-v = V[0.25, 0.25, 0.25, 0.25, -sqrt(3)/8, -sqrt(3)/8]
-# g = G[(0.0, 0.0), (1.0, 0.0), (0.0, 1.0), (3.0, 2.0), (0.0, 6.0), (9.0, 12.0)]
+v = V[0.25 0.25 0.25 0.25 -sqrt(3)/8 -sqrt(3)/8]
+g = G[(-0.5, -0.5) (0.5, -0.5) (-0.5, 0.5) (0.5, 0.5) (sqrt(3)/4, 0.0) (-sqrt(3)/4, 0.0)]
+h = H[(0.0, 1.0, 1.0, 0.0) (0.0, -1.0, -1.0, 0.0) (0.0, -1.0, -1.0, 0.0) (0.0, 1.0, 1.0, 0.0) (0.0, 0.0, 0.0, sqrt(3)) (0.0, 0.0, 0.0, sqrt(3))]
 
-bx = repeat(permutedims(v),np)
-# ∇bx = repeat(permutedims(g),np)
-# test_field_array(b,x,bx,cmp=(.≈))
+@test evaluate(b,[xi,]) ≈ v
+@test evaluate(∇b,[xi,]) ≈ g
+@test evaluate(∇∇b,[xi,]) ≈ h
 
 # Vector-valued Q space with isotropic order
 
@@ -122,10 +113,32 @@ bx = repeat(permutedims(v),np)
 Hbx = repeat(permutedims(h),np)
 test_field_array(b,x,bx,grad=∇bx,gradgrad=Hbx)
 
-# order = 1
-# b = MonomialBasis{1}(Float64,order)
-# @test evaluate(b,Point{1,Float64}[(0,),(1,)]) == [1.0 0.0; 1.0 1.0]
+# Degenerated case 1: order 0
 
+# xi = Point(2,3)
+# np = 5
+# x = fill(xi,np)
+
+# order = 0
+# V = Float64
+# G = gradient_type(V,xi)
+# H = gradient_type(G,xi)
+# b = MonomialBasis{2}(V,order)
+# @test get_order(b) == 0
+# @test get_orders(b) == (0,0)
+
+# v = V[1.0,]
+# g = G[(0.0, 0.0),]
+# h = H[(0.0, 0.0, 0.0, 0.0),]
+
+# bx = repeat(permutedims(v),np)
+# ∇bx = repeat(permutedims(g),np)
+# Hbx = repeat(permutedims(h),np)
+# test_field_array(b,x,bx,grad=∇bx,gradgrad=Hbx)
+
+# Degenerated case 2: void Point instance
+
+# order = 1
 # b = MonomialBasis{0}(VectorValue{2,Float64},order)
 # @test evaluate(b,Point{0,Float64}[(),()]) == VectorValue{2,Float64}[(1.0, 0.0) (0.0, 1.0); (1.0, 0.0) (0.0, 1.0)]
 
